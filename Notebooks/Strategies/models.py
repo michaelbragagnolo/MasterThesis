c22f815d6ebb1f2597a949b39cff025760ca1ebd
Michael <36728184+michaelbragagnolo@users.noreply.github.com>
2022-02-03 23:15:46 +0100

    Testing LwF

--- Notebooks/Strategies/models.py
+++ Notebooks/Strategies/models.py
@@ -1,54 +1,30 @@
-from avalanche.models import MultiHeadClassifier, MultiTaskModule
 from torch import nn
 
-
-class MultiHeadMLP(MultiTaskModule):
-    def __init__(self, input_size=28 * 28, hidden_size=256, hidden_layers=2,
-                 drop_rate=0, relu_act=True):
-        super().__init__()
-        self._input_size = input_size
-
-        layers = nn.Sequential(*(nn.Linear(input_size, hidden_size),
-                                 nn.ReLU(inplace=True) if relu_act else nn.Tanh(),
-                                 nn.Dropout(p=drop_rate)))
-        for layer_idx in range(hidden_layers - 1):
-            layers.add_module(
-                f"fc{layer_idx + 1}", nn.Sequential(
-                    *(nn.Linear(hidden_size, hidden_size),
-                      nn.ReLU(inplace=True) if relu_act else nn.Tanh(),
-                      nn.Dropout(p=drop_rate))))
-
-        self.features = nn.Sequential(*layers)
-        self.classifier = MultiHeadClassifier(hidden_size)
-
-    def forward(self, x, task_labels):
-        x = x.contiguous()
-        x = x.view(x.size(0), self._input_size)
-        x = self.features(x)
-        x = self.classifier(x, task_labels)
-        return x
-
-
+# Class used to define NN model
+# Multilayer perceptron model (feedforward network)
 class MLP(nn.Module):
+    # flatten the input image
+    # linear layers (fully connected)
+    # dropout and activation function
     def __init__(self, input_size=28 * 28, hidden_size=256, hidden_layers=2,
                  output_size=10, drop_rate=0, relu_act=True):
         super().__init__()
         self._input_size = input_size
 
-        layers = nn.Sequential(*(nn.Linear(input_size, hidden_size),
-                                 nn.ReLU(inplace=True) if relu_act else nn.Tanh(),
-                                 nn.Dropout(p=drop_rate)))
+        layers = nn.Sequential(*(nn.Linear(input_size, hidden_size),                # weighted sum of the inputs
+                                 nn.ReLU(inplace=True) if relu_act else nn.Tanh(),  # apply ReLU activation function elementwise (or hyperbolic tan)
+                                 nn.Dropout(p=drop_rate)))  # Dropout rate (randomly selected neurons ignored during training)
         for layer_idx in range(hidden_layers - 1):
-            layers.add_module(
-                f"fc{layer_idx + 1}", nn.Sequential(
-                    *(nn.Linear(hidden_size, hidden_size),
-                      nn.ReLU(inplace=True) if relu_act else nn.Tanh(),
-                      nn.Dropout(p=drop_rate))))
+            layers.add_module(f"fc{layer_idx + 1}",
+                              nn.Sequential(*(nn.Linear(hidden_size, hidden_size),
+                              nn.ReLU(inplace=True) if relu_act else nn.Tanh(),
+                              nn.Dropout(p=drop_rate))))
 
-        self.features = nn.Sequential(*layers)
-        self.classifier = nn.Linear(hidden_size, output_size)
+        self.features = nn.Sequential(*layers)                  # holds output of the model w/o last classification model
+        self.classifier = nn.Linear(hidden_size, output_size)   # feedforward network
 
-    def forward(self, x):
+    # Define how to forward propagate input through the defined layers
+    def forward(self, x): # [batch size, height, width]
         x = x.contiguous()
         x = x.view(x.size(0), self._input_size)
         x = self.features(x)
@@ -56,33 +32,4 @@ class MLP(nn.Module):
         return x
 
 
-class SI_CNN(MultiTaskModule):
-    def __init__(self, hidden_size=512):
-        super().__init__()
-        layers = nn.Sequential(*(nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=(1, 1)),
-                                 nn.ReLU(inplace=True),
-                                 nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3)),
-                                 nn.ReLU(inplace=True),
-                                 nn.MaxPool2d((2, 2)),
-                                 nn.Dropout(p=0.25),
-                                 nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=(1, 1)),
-                                 nn.ReLU(inplace=True),
-                                 nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3)),
-                                 nn.ReLU(inplace=True),
-                                 nn.MaxPool2d((2, 2)),
-                                 nn.Dropout(p=0.25),
-                                 nn.Flatten(),
-                                 nn.Linear(2304, hidden_size),
-                                 nn.ReLU(inplace=True),
-                                 nn.Dropout(p=0.5)
-                                 ))
-        self.features = nn.Sequential(*layers)
-        self.classifier = MultiHeadClassifier(hidden_size, initial_out_features=10)
-
-    def forward(self, x, task_labels):
-        x = self.features(x)
-        x = self.classifier(x, task_labels)
-        return x
-
-
-__all__ = ['MultiHeadMLP', 'MLP', 'SI_CNN']
+__all__ = ['MLP']
